2e981197e067def2bb3174389a7da311b693ae76
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-14 14:41:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ var emptyCart = [];
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
-    
+
     console.log("Your total cost is $" + total + ", which will be charged to the card " + cardNumber + ".");
     setCart(emptyCart)
     return cart
