894ed197128bf8f1f2875fb85462c03532770799
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-14 14:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,8 +55,9 @@ function removeFromCart(item) {
   if (cart[i].hasOwnProperty(item)) {
     cart.splice(i, 1);
   } else {
-   console.log("That item is not in your cart.");}
-  }
+   console.log("That item is not in your cart.");
+ }
+}
   return cart
 }
 
