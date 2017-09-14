bfed736a81b0146a743bd024b9bf709730fe0d1f
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-14 14:42:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ var emptyCart = [];
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
 
-    console.log("Your total cost is $" + total + ", which will be charged to the card " + cardNumber + ".");
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     setCart(emptyCart)
     return cart
   }
