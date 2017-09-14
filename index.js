4dda9e37bdc1d7cbdb56ce3c629be33259f7e054
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-14 14:38:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,9 +61,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+var emptyCart = [];
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else  {
     console.log("Your total cost is $" + total + ", which will be charged to the card " + cardNumber+ ".");
+    setCart(emptyCart)
+    return cart
   }
 }
