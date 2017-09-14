4788768960c465315ea314ff3ee01a6c73b5bdbf
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-14 14:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
 var emptyCart = [];
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
-  } else if {
+  } else {
     
     console.log("Your total cost is $" + total + ", which will be charged to the card " + cardNumber + ".");
     setCart(emptyCart)
